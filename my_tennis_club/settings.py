ed52168f1bc8b5fee35ab00ae147f1dcc9ef33c5
Master-Winner <armenhadzic0516@gmail.com>
2024-12-02 17:45:11 +0200

    Add 404 Page

--- my_tennis_club/settings.py
+++ my_tennis_club/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-idh+7u6c0fikr%%rvgub!7-czd9hg=3ousnfuz9vo!)q_!k+1k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
